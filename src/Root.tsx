1ca629df608a22905a76d8cae951c43a45aecf52
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-03-08 06:28:24 -0700

    feat[toolnav]: add icons

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { RiCursorFill } from 'react-icons/ri';
+import { BiSquare } from 'react-icons/bi';
+import { BsArrowRight, BsDiamond, BsFillPencilFill, BsImage } from 'react-icons/bs';
+import { ImTextColor } from 'react-icons/im';
 
 export const Root = () => {
   return (
     <main>
       <nav className="toolnav flex flex-row gap-1 p-2 fixed top-2 mx-auto inset-x-0 w-min shadow rounded-lg">
-        <button className="select rounded h-10 w-10">Se</button>
-        <button className="square rounded h-10 w-10">Sq</button>
-        <button className="diamond rounded h-10 w-10">Di</button>
-        <button className="arrow rounded h-10 w-10">Ar</button>
-        <button className="freedraw rounded h-10 w-10">Fd</button>
-        <button className="text rounded h-10 w-10">Tx</button>
-        <button className="img rounded h-10 w-10">Im</button>
+        <button className="select rounded h-10 w-10 flex items-center justify-center"><RiCursorFill /></button>
+        <button className="square rounded h-10 w-10 flex items-center justify-center"><BiSquare /></button>
+        <button className="diamond rounded h-10 w-10 flex items-center justify-center"><BsDiamond /></button>
+        <button className="arrow rounded h-10 w-10 flex items-center justify-center"><BsArrowRight /></button>
+        <button className="freedraw rounded h-10 w-10 flex items-center justify-center"><BsFillPencilFill /></button>
+        <button className="text rounded h-10 w-10 flex items-center justify-center"><ImTextColor /></button>
+        <button className="img rounded h-10 w-10 flex items-center justify-center"><BsImage /></button>
       </nav>
     </main>
   );
