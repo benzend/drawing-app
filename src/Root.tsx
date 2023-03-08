00656338586aafbb7717c162fff7762adaa17b24
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-03-08 06:17:34 -0700

    style[toolnav]: center

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Root = () => {
   return (
     <main>
-      <nav className="toolnav flex flex-row gap-1 p-2 absolute shadow rounded-lg">
+      <nav className="toolnav flex flex-row gap-1 p-2 fixed top-2 mx-auto inset-x-0 w-min shadow rounded-lg">
         <button className="select rounded h-10 w-10">Se</button>
         <button className="square rounded h-10 w-10">Sq</button>
         <button className="diamond rounded h-10 w-10">Di</button>
