e2a0084c1810bb4ac4eced7ff3c46739b860c4d0
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-03-07 22:35:11 -0700

    chore[tool nav]: basic nav structure

---
+++ src/Root.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Root = () => {
+  return (
+    <main>
+      <nav className="toolnav">
+        <button className="select">Se</button>
+        <button className="square">Sq</button>
+        <button className="diamond">Di</button>
+        <button className="arrow">Ar</button>
+        <button className="freedraw">Fd</button>
+        <button className="text">Tx</button>
+        <button className="img">Im</button>
+      </nav>
+    </main>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import { Root } from './Root';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  }
+    element: <Root />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
